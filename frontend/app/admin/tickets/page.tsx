24c0b67995c5c955b3b7d04f8d1d0750b8ec5f78
JaydeMike <engraciajayde@gmail.com>
2026-01-11 10:14:54 +0800

    Added table

--- frontend/app/admin/tickets/page.tsx
+++ frontend/app/admin/tickets/page.tsx
@@ -1,15 +1,34 @@
 import AdminHeader from '@/app/components/adminHeader'
 import React from 'react'
+import { columns, Payment } from './columns'
+import { DataTable } from './data-table'
 
-const tickets = () => {
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+export default async function tickets() {
+  const data = await getData()
+
+  
   return (
     <div>
       <AdminHeader/>
 
-      
+      <div className="container mx-auto mt-10">
+        <DataTable columns={columns} data={data} />
+      </div>
 
     </div>
   )
 }
 
-export default tickets
