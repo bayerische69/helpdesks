cfb1e02f93aeaaaf30e44128279a16839f0a0f07
JaydeMike <engraciajayde@gmail.com>
2026-01-14 21:44:11 +0800

    Added settings page

---
+++ frontend/app/admin/settings/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminHeader from '@/app/components/adminHeader'
+
+const settings = () => {
+  return (
+    <div>
+        <AdminHeader />
+
+        
+    </div>
+  )
+}
+
+export default settings
